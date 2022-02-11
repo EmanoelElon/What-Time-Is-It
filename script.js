e07da34f1a81beea52812409041e4ed342c69edb
Emanoel Elon <emanoelefgms@gmail.com>
2022-02-11 08:40:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-/*const today = new Date();
+window.onload = function() {
+  let aTime = document.getElementById("time");
 
-let hour = today.getHours();
-let min = today.getMinutes();
-
-let time = hour + ":" + min;
-
-console.log(hour + " : " + min);
-
-var x = (document.getElementById("hour").innerHTML = time);*/
-
-let aTime = document.getElementById("time");
-
-setInterval(function () {
-  var currentTime = new Date(),
+  setInterval(function () {
+    var currentTime = new Date(),
     hours = currentTime.getHours(),
     minutes = currentTime.getMinutes();
 
-  /*hours += hours < 10 ? "0" : "";
-  minutes += minutes < 10 ? "0" : "";*/
+    /*hours += hours < 10 ? "0" : "";
+    minutes += minutes < 10 ? "0" : "";*/
 
-  aTime.innerHTML = addZeroes(hours, 2) + " " + addZeroes(minutes, 2);
-}, 1000);
+    aTime.innerHTML = addZeroes(hours, 2) + " " + addZeroes(minutes, 2);
+  }, 1000);
 
-function addZeroes(num, len) {
-  var numberWithZeroes = String(num);
-  var counter = numberWithZeroes.length;
+  function addZeroes(num, len) {
+    var numberWithZeroes = String(num);
+    var counter = numberWithZeroes.length;
 
-  while (counter < len) {
-    numberWithZeroes = "0" + numberWithZeroes;
+    while (counter < len) {
+      numberWithZeroes = "0" + numberWithZeroes;
 
-    counter++;
+      counter++;
+    }
+
+    return numberWithZeroes;
   }
-
-  return numberWithZeroes;
 }
